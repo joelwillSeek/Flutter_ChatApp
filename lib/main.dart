88f9550be199d087c58503eec13f94db9119f675
joel <joelthousend@gmail.com>
2024-03-01 16:11:03 +0300

    added framework of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chatapp/chat_history.dart';
 import 'package:flutter_chatapp/messageAndSendButton.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final List<Map<String, String>> chatHistory = [
+    {
+      "message": "message",
+      "date": "month/day/year",
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Column(
           children: [
-            GetChatHistory(),
-            MessageAndSendButton(),
+            Expanded(
+              flex: 5,
+              child: ListView.builder(
+                itemCount: chatHistory.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(children: [
+                      Text(chatHistory[index]["message"].toString()),
+                      Text(chatHistory[index]["date"].toString()),
+                    ]),
+                  );
+                },
+              ),
+            ),
+            const MessageAndSendButton()
           ],
         ),
       ),
